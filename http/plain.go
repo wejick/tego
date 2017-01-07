005e1bfb5e08dacf8742ea765d7eaf5b458c1e38
Gian Giovani <wejick@gmail.com>
2017-01-07 14:30:30 +0700

    plain is plain

--- http/plain.go
+++ http/plain.go
@@ -9,14 +9,10 @@ import (
 func ResponsePlain(w http.ResponseWriter, message string, code int, callback string) {
 	bytedMessage := []byte(message[:])
 	if callback != "" {
-		w.Header().Set("Content-Type", "text/javascript")
 		fmt.Fprintf(w, "%s(%s)", callback, bytedMessage)
 		return
 	}
 
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-
-	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(code)
 	w.Write(bytedMessage)
 }
@@ -24,9 +20,7 @@ func ResponsePlain(w http.ResponseWriter, message string, code int, callback str
 //ResponsePlainCode middle to write http header code to header
 func ResponsePlainCode(w http.ResponseWriter, message string, code int) {
 	bytedMessage := []byte(message[:])
-	w.Header().Set("Access-Control-Allow-Origin", "*")
 
-	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(code)
 	w.Write(bytedMessage)
 }
